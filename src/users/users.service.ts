5ff20de81bb64bca0764f626c5133f31b970f823
Diana <di.harchenko@istu.edu.ua>
2024-06-20 00:17:02 +0300

    init

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  public users: User[] = [
+    {id: 1, name: 'Diana', email: 'Diana@gmail.com', subscription: true}, 
+    {id: 2, name: 'Zaur', email: 'Zaur@gmail.com', subscription: false},
+];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find(user => user.id === id);
+    if (!user) {
+        throw new NotFoundException(`user with id: ${id} not found`);
+    }
+    return user;    
+  }
+}
